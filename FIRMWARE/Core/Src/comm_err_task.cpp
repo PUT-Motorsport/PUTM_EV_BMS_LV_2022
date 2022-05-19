b633105f1ddfc08eb12b0d2c57655de2ff9d7fc4
Maksymilian Jaruga <maksymilian.jaruga@gmail.com>
2022-05-19 18:20:22 +0200

    comm_err_task update

--- FIRMWARE/Core/Src/comm_err_task.cpp
+++ FIRMWARE/Core/Src/comm_err_task.cpp
@@ -5,7 +5,7 @@
  *      Author: Maks
  */
 #include <comm_err_task.hpp>
-#include <etl/flat_map.h>
+#include <etl/vector.h>
 
 enum struct Error_condition{
 	UNBALANCE,
@@ -18,77 +18,61 @@ enum struct Error_condition{
 };
 
 
+
 struct Error_and_connditions{
 	Error_condition error;
 	float min;
 	float max;
-	//const float &value;
+	float value;
+	const uint32_t error_time;
+	uint32_t timer;
+	bool flag;
 };
 
 
 // error if value is in range <min, max>
-std::array< Error_and_connditions, 7> error_conditions = {
-		Error_and_connditions{Error_condition::UNBALANCE,{2000,INT_MAX}},
-		Error_and_connditions{Error_condition::VOLTAGE_LOW,{INT_MIN,30000}},
-		Error_and_connditions{Error_condition::VOLTAGE_HIGH,{42200,INT_MAX}},
-		Error_and_connditions{Error_condition::TEMPERATURE_WARNING,{48,55}},
-		Error_and_connditions{Error_condition::TEMPERATURE_HIGH,{55,INT_MAX}},
-		Error_and_connditions{Error_condition::CURRENT_HIGH,{20,INT_MAX}},
-		Error_and_connditions{Error_condition::NEUTRAL_CURRENT_CAR,{0,1}} //float?  //to check
+Error_and_connditions error_conditions[7] = {
+		{Error_condition::UNBALANCE,2000,50000,(float)(data.voltages.highest_cell_voltage-data.voltages.lowest_cell_voltage),500},
+		{Error_condition::VOLTAGE_LOW,0,30000,(float)data.voltages.lowest_cell_voltage,500},
+		{Error_condition::VOLTAGE_HIGH,42200,500000,(float)data.voltages.highest_cell_voltage,500},
+		{Error_condition::TEMPERATURE_WARNING,48,55,(float)data.temperatures.highest_temperature,1000},
+		{Error_condition::TEMPERATURE_HIGH,55,120,(float)data.temperatures.highest_temperature,500},
+		{Error_condition::CURRENT_HIGH,20,100,data.current.value,500},
+		{Error_condition::NEUTRAL_CURRENT_CAR,0,0.3,data.current.value,1200000} //to check
 };
 
 
 
-void error_values(){
+void error_check(){
 
-//	Error_condition::UNBALANCE = data.voltages.cells[i] - data.voltages.lowest_cell_voltage;
-//	Error_condition::VOLTAGE_LOW = data.voltages.lowest_cell_voltage;
-//	Error_condition::VOLTAGE_LOW = data.voltages.highest_cell_voltage;
-//	Error_condition::TEMPERATURE_WARNING = data.temperatures.highest_temperature;
-//	Error_condition::TEMPERATURE_HIGH = data.temperatures.highest_temperature;
-//	Error_condition::CURRENT_HIGH = data.current.value;
-//	Error_condition::NEUTRAL_CURRENT_CAR = data.current.value;
 
-	etl::vector<Error_condition, 10> errors;
+	etl::vector<Error_condition, 10> errors_vector;
 
-	for( const auto& error : error_conditions){
+	for(auto& error : error_conditions){
 		if(error.min <= error.value && error.value <= error.max){
-			errors.emplace_back(error.error);
+
+			if(false == error.flag)
+			{
+				errors_vector.emplace_back(error.error);
+				error.timer = HAL_GetTick() + error.error_time;
+				error.flag = true;
+				//acu state
+			}
+		}
+		else{
+			error.timer = HAL_GetTick() + error.error_time;
+			error.flag = false;
+			//acu state
 		}
 	}
 
-	for (auto error : errors){
+	for (auto error : errors_vector){
 		// do sth
 	}
 
 
 }
 
-
-
-struct Timers{
-	uint32_t balance_error[NUMBER_OF_CELLS];
-	uint32_t low_voltage_error[NUMBER_OF_CELLS];
-	uint32_t high_voltage_error[NUMBER_OF_CELLS];
-	uint32_t high_temperature_error[NUMBER_OF_CELLS];
-	uint32_t high_current_error[1];
-	uint32_t sleep[1];
-
-};
-
-struct Flags{
-	bool balance_error[NUMBER_OF_CELLS];
-	bool low_voltage_error[NUMBER_OF_CELLS];
-	bool high_voltage_error[NUMBER_OF_CELLS];
-	bool high_temperature_error[NUMBER_OF_CELLS];
-	bool high_current_error[1];
-	bool sleep[1];
-};
-
-struct Error{
-	Timers timers;
-	Flags flags;
-}error;
 
 /*
 	if(timer <= HAL_GetTick())
@@ -98,80 +82,6 @@ struct Error{
 		// led
 	}
  */
-
-void if_error_occurs(uint32_t *timers, bool *flag, unsigned int *i, static const int condition, static const int error_time, uint16_t variable)
-{
-	switch (condition)
-	{
-	case 'UNBALANCE_LIMIT':
-	case 'VOL_UP_LIMIT':
-	case 'TOO_HIGH_CURRENT':
-	case 'TEMPERATURE_LIMIT':
-	case 'TEMPERATURE_WARNING':
-		if(variable > condition)
-		{
-			if(false == flag[i])
-			{
-				timers[i] = HAL_GetTick() + error_time;
-				flag[i] = true;
-				//acu state
-			}
-		}else{
-			timers[i] = HAL_GetTick() + error_time;
-			flag[i] = false;
-			//acu state
-		}
-
-
-		break;
-	case 'VOL_DOWN_LIMIT':
-	case 'NEUTRAL_CURRENT_CAR': //when the car is turned off, it causes counting to program sleep
-		if(variable < condition)
-		{
-			if(false == flag[i])
-			{
-				timers[i] = HAL_GetTick() + error_time;
-				flag[i] = true;
-				//acu state
-			}
-		}else{
-			timers[i] = HAL_GetTick() + error_time;
-			flag[i] = false;
-			//acu state
-		}
-
-
-
-		break;
-
-	}
-}
-
-void error_check()
-{
-	for(unsigned int i = 0; i < NUMBER_OF_CELLS; i++)
-	{
-		uint16_t voltage_diff = data.voltages.cells[i] - data.voltages.lowest_cell_voltage;
-
-		if_error_occurs(error.timers.balance_error, error.flags.balance_error, i, UNBALANCE_LIMIT, ERROR_TIME, voltage_diff);
-
-		if_error_occurs(error.timers.low_voltage_error, error.flags.low_voltage_error, i, VOL_DOWN_LIMIT, ERROR_TIME, data.voltages.cells);
-
-		if_error_occurs(error.timers.high_voltage_error, error.flags.high_voltage_error, i, VOL_UP_LIMIT, ERROR_TIME, data.voltages.cells);
-
-		if(5 != i)
-		{
-			if_error_occurs(error.timers.high_temperature_error, error.flags.high_temperature_error, i, TEMPERATURE_LIMIT, ERROR_TIME_TEMPERATURES, data.temperatures.values);
-
-			//should I add a warning for lower temperature?
-			//if_error_occurs(error.timers.high_temperature_error, error.flags.high_temperature_error, i, TEMPERATURE_LIMIT, ERROR_TIME_TEMPERATURES, data.temperatures.values);
-		}
-	}
-
-	if_error_occurs(error.timers.high_current_error, error.flags.high_current_error, 0, TOO_HIGH_CURRENT, ERROR_TIME, (uint16_t)data.current.value);
-
-
-}
 
 void start_comm_function(void *argument){
 
--- FIRMWARE/Debug/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/subdir.mk
+++ FIRMWARE/Debug/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/subdir.mk
@@ -15,13 +15,13 @@ OBJS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/%.o Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/%.su: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/%.c Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/subdir.mk
-	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L4P5xx -c -I../Core/Inc -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Drivers/CMSIS/Include -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I"/home/jan/Documents/PUT-Motorsport/PUTM_EV_BMS_LV_2022/FIRMWARE/Drivers/ETL/include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/%.o: ../Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/%.c Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32L4P5xx -c -I../Core/Inc -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F -I../Drivers/CMSIS/Include -I../Drivers/CMSIS/Device/ST/STM32L4xx/Include -I../Drivers/STM32L4xx_HAL_Driver/Inc -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Drivers/STM32L4xx_HAL_Driver/Inc/Legacy -I"F:/GitHub/PUTM_EV_BMS_LV_2022/FIRMWARE/Drivers/ETL/include" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
 
 clean: clean-Middlewares-2f-Third_Party-2f-FreeRTOS-2f-Source-2f-portable-2f-MemMang
 
 clean-Middlewares-2f-Third_Party-2f-FreeRTOS-2f-Source-2f-portable-2f-MemMang:
-	-$(RM) ./Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.d ./Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.o ./Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.su
+	-$(RM) ./Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.d ./Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.o
 
 .PHONY: clean-Middlewares-2f-Third_Party-2f-FreeRTOS-2f-Source-2f-portable-2f-MemMang
 
